e0849f34db8d9a289e80c2dcdbce8e232913332e
YajieGu <yj-se7en@live.com>
2020-07-14 19:17:44 +0100

    cropping

--- ddfa.py
+++ ddfa.py
@@ -8,11 +8,15 @@ def load_file():
     w_exp = np.load("./3ddfa_data/w_exp_sim.npy")
     u_shp = np.load("./3ddfa_data/u_shp.npy")
     w_shp = np.load("./3ddfa_data/w_shp_sim.npy")
-    # triangle_data = loadmat("./3ddfa_data/template_data.mat")
+    subset = loadmat("./3ddfa_data/bfm_subset(29495).mat")
+    subset_info = subset["subset_info"]
+    shape_index = subset_info["shape_index"][0][0]
+    face_index = subset_info["face_index"][0][0]
+    face = subset_info["face"][0][0]
     # triangle = triangle_data["mean_face"]
     triangle_data = loadmat("./3ddfa_data/tri.mat")
-    triangle = triangle_data["tri"].T
-    return [u_exp, w_exp, u_shp, w_shp, triangle]
+    tri_origin = triangle_data["tri"].T
+    return [u_exp, w_exp, u_shp, w_shp, shape_index, face_index, face, tri_origin]
 
 
 def shp_exp_generate():
@@ -21,12 +25,22 @@ def shp_exp_generate():
     sigma = 1
     vertices = []
     param_list = []
-    # vertex_list = np.zeros(shape=(29495, 3))
-    # vertex_list = np.zeros(shape=(53215, 3))
+    vertex_list = np.zeros(shape=(29495, 3))
+    # triangle = np.zeros(shape=(58366, 1))
     exp_mapping_list = [np.zeros(shape=(53215, 3))]
 
-    [u_exp, w_exp, u_shp, w_shp, triangle] = load_file()
-    triangle = triangle.astype(np.int)
+    [u_exp, w_exp, u_shp, w_shp, shape_index, face_index, face, tri_origin] = load_file()
+
+    shape_index = shape_index.astype(np.int)
+    # triangle = triangle.astype(np.int)
+
+    '''
+    # crop redundant triangles (from 100000+ to 50000+)
+    for index in face_index:
+        index = index - 1
+        triangle = tri_origin[index]
+    '''
+
 
     # generate random 6 expressions per subject
     for i in range(6):
@@ -60,26 +74,22 @@ def shp_exp_generate():
         exp_mapping = exp_mapping_list[i]
         vertex = u + shp_mapping + exp_mapping
 
-        '''
         # crop redundant vertices (from 50000+ to 29000+)
         j = 0
-        for k in range(triangle.max()):
-            vertex_list[j] = vertex[k]
+        for k in shape_index:
+            vertex_list[j] = vertex[k-1]
             j += 1
-        '''
 
         # Normalisation.
         # Transform to origin.
-        vertex = vertex - np.mean(vertex, axis=0)
-        # vertex_list = vertex_list - np.mean(vertex_list, axis=0)
+        vertex_list = vertex_list - np.mean(vertex_list, axis=0)
 
         # Find distances to origin and do normalisation.
-        # vertex_list_norm = vertex_list / np.max(np.linalg.norm(vertex_list, axis=1))
-        vertex_list_norm = vertex / np.max(np.linalg.norm(vertex, axis=1))
+        vertex_list_norm = vertex_list / np.max(np.linalg.norm(vertex_list, axis=1))
 
         vertices.append(vertex_list_norm)
 
-    return [vertices, triangle, shp_mapping, exp_mapping_list]
+    return [vertices, face, shp_mapping, exp_mapping_list]
 
 
 def write_obj(subject_num):
